714eda1ef27e76dea0c3debfdde75e87bc0c3f8b
Alex Kite <alexkite511@gmail.com>
2018-04-27 01:29:15 -0700

    file io working with command line arguments

--- FileIO.rb
+++ FileIO.rb
@@ -2,13 +2,15 @@ require_relative 'IntervalSearchTree'
 
 require 'csv'
 # require 'file'
-
-csv_path = File.join(File.dirname(__FILE__), 'inputfile.csv')
+puts ARGV[0]
+# csv_path = File.join(File.dirname(__FILE__), 'inputfile.csv')
+csv_path = ARGV[0]
 
 file = File.read(csv_path)
 csv = CSV.parse(file)
 
-txt_file = File.new("output.txt", "w")
+# txt_file = File.new("fsastuff.txt", "w")
+txt_file = File.new(ARGV[1], "w")
 
 tree = IntervalSearchTree.new
 
@@ -28,3 +30,4 @@ csv.each do |row|
   end
   txt_file.puts(tree.in_order_traversal.to_s)
 end
+# /Users/alexkite/Desktop/inputfile.csv
